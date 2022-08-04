4f9021815c2378423b9fc272390bb497b244dd94
Konrad Beckmann <konrad.beckmann@gmail.com>
2022-08-04 02:22:27 +0200

    Configure cmake to use riscv-none-embed-objcopy
    The default objcopy found by cmake may not support riscv32.

--- toolchain-rv32.cmake
+++ toolchain-rv32.cmake
@@ -3,6 +3,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER riscv-none-embed-gcc)
 set(CMAKE_CXX_COMPILER riscv-none-embed-g++)
 set(CMAKE_ASM_COMPILER riscv-none-embed-gcc)
+set(CMAKE_OBJCOPY riscv-none-embed-objcopy)
 
 set(CMAKE_C_FLAGS "-march=rv32imc" CACHE STRING "C Compiler Base Flags")
 set(CMAKE_CXX_FLAGS "-march=rv32imc" CACHE STRING "C++ Compiler Base Flags")
