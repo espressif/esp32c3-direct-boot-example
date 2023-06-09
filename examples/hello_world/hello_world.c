ef26955e40e2af8223aa86652326fb9368f6c222
Ivan Grokhotkov <ivan@espressif.com>
2023-06-09 17:49:22 +0200

    move hal library to top level, disable WDTs in examples

--- examples/hello_world/hello_world.c
+++ examples/hello_world/hello_world.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include "hal/wdt_hal.h"
+
+static void delay(void);
 
 int main(void)
 {
-    printf("Hello, world!\n");
+    // Disable the watchdogs
+    wdt_hal_context_t mwdt_ctx = {.inst = WDT_MWDT0, .mwdt_dev = &TIMERG0};
+    wdt_hal_write_protect_disable(&mwdt_ctx);
+    wdt_hal_disable(&mwdt_ctx);
+    wdt_hal_set_flashboot_en(&mwdt_ctx, false);
+    wdt_hal_context_t rwdt_ctx = RWDT_HAL_CONTEXT_DEFAULT();
+    wdt_hal_write_protect_disable(&rwdt_ctx);
+    wdt_hal_disable(&rwdt_ctx);
+    wdt_hal_set_flashboot_en(&rwdt_ctx, false);
+    // Super WDT is still enabled; no HAL API for it yet
+
+    while(1) {
+        printf("Hello, world!\n");
+        delay();
+    }
     return 0;
+}
+
+static void delay(void)
+{
+    for (int i = 0; i < 300000; i++) {
+        asm volatile ("nop");
+    }
 }
